12642d882da7bb894238e182ff8a0631e93b950d
Justin Nappi <justin.nappi8@gmail.com>
2025-05-27 16:08:33 -0400

    started lessons table

--- src/db/schema/lesson.ts
+++ src/db/schema/lesson.ts
@@ -1,6 +1,14 @@
-import { pgTable, uuid } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, timestamp, uuid } from "drizzle-orm/pg-core";
 
+import armCare from "./assessments/arm-care";
 import users from "./users";
+
+export const lessonTypes = pgEnum("lesson_types", [
+  "strength",
+  "hitting",
+  "pithing",
+  "fielding",
+]);
 
 const lesson = pgTable("lesson", {
   id: uuid("id").primaryKey().defaultRandom(),
@@ -10,5 +18,11 @@ const lesson = pgTable("lesson", {
   coachId: uuid("coachId")
     .notNull()
     .references(() => users.id, { onDelete: "cascade" }),
+  type: lessonTypes("type").notNull(),
+  armCare: uuid("armCare").references(() => armCare.id, {
+    onDelete: "cascade",
+  }),
+  createdOn: timestamp("created_on", { mode: "date" }).notNull().defaultNow(),
+  lessonDate: timestamp("lesson_date", { mode: "date" }).notNull(),
 });
 export default lesson;
