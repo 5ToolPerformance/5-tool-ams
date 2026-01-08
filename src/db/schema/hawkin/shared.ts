e50451ab762ef4a511e6931f0292f412e5964cee
Justin Nappi <justin.nappi8@gmail.com>
2026-01-08 12:47:14 -0500

    fix: Hawkin Schema Names

--- src/db/schema/hawkin/shared.ts
+++ src/db/schema/hawkin/shared.ts
@@ -3,12 +3,12 @@ import { jsonb, text, timestamp, uuid } from "drizzle-orm/pg-core";
 export const baseHawkinsColumns = {
   id: uuid("id").defaultRandom().primaryKey(),
   timestamp: timestamp("timestamp", { withTimezone: true }).notNull(),
-  externalUniqueId: text("external_uniqueId").notNull(),
+  externalUniqueId: text("external_unique_id").notNull(),
   athleteId: text("athlete_id").notNull(),
   athleteName: text("athlete_name"),
-  testTypeName: text("testType_name").notNull(),
-  testTypeCanonicalId: text("testType_canonicalId").notNull(),
-  rawData: jsonb("rawData").notNull(),
+  testTypeName: text("test_type_name").notNull(),
+  testTypeCanonicalId: text("test_type_canonical_id").notNull(),
+  rawData: jsonb("raw_data").notNull(),
   ingestedAt: timestamp("ingested_at", { withTimezone: true })
     .defaultNow()
     .notNull(),
