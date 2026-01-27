378a549c4ec9cce02e84dea078edd886b2505215
Justin Nappi <justin.nappi8@gmail.com>
2026-01-27 10:19:41 -0500

    feat: added type option to notes and applied schema migration

--- src/db/schema/players/playerNotes.ts
+++ src/db/schema/players/playerNotes.ts
@@ -1,25 +1,26 @@
+import { relations } from "drizzle-orm";
 import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
 import { playerInformation, users } from "..";
 
 export const playerNotes = pgTable("player_notes", {
-    id: uuid("id").primaryKey().defaultRandom(),
-    playerId: uuid("player_id").references(() => playerInformation.id),
-    authorId: uuid("author_id").references(() => users.id),
-    content: text("content").notNull(),
-    visibility: text("visibility").notNull().default("private"),
-    createdAt: timestamp("created_at", { mode: "string" }).notNull().defaultNow(),
+  id: uuid("id").primaryKey().defaultRandom(),
+  playerId: uuid("player_id").references(() => playerInformation.id),
+  authorId: uuid("author_id").references(() => users.id),
+  content: text("content").notNull(),
+  type: text("type"),
+  visibility: text("visibility").notNull().default("private"),
+  createdAt: timestamp("created_at", { mode: "string" }).notNull().defaultNow(),
 });
 
-import { relations } from "drizzle-orm";
-
 export const playerNotesRelations = relations(playerNotes, ({ one }) => ({
-    author: one(users, {
-        fields: [playerNotes.authorId],
-        references: [users.id],
-    }),
+  author: one(users, {
+    fields: [playerNotes.authorId],
+    references: [users.id],
+  }),
 
-    player: one(playerInformation, {
-        fields: [playerNotes.playerId],
-        references: [playerInformation.id],
-    }),
+  player: one(playerInformation, {
+    fields: [playerNotes.playerId],
+    references: [playerInformation.id],
+  }),
 }));
