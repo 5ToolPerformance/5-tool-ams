ec54a019934fe5e86572e6fd986713114ab95a39
Justin Nappi <justin.nappi8@gmail.com>
2026-03-09 15:14:17 -0400

    dev: working on implementing queries and domain logic for eval pipeline

---
+++ src/db/queries/evaluations/createEvaluation.ts
@@ -0,0 +1,6 @@
+import { DB } from "@/db";
+import { EvaluationInput } from "@/domain/evaluations/validateEvaluationInput";
+
+export async function createEvaluation(db: DB, input: EvaluationInput) {
+  // TODO
+}
